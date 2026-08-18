329716196f7270e5014cee03524f444d80d1e243
Julieta Wiesztort <julieta.raw@gmail.com>
2026-08-18 13:55:35 -0300

    chore(config): point API to production backend

--- src/app/core/api/environment.ts
+++ src/app/core/api/environment.ts
@@ -1,2 +1,6 @@
-export const API_BASE_URL = 'http://localhost:8000';
+// dev:
+// export const API_BASE_URL = 'http://localhost:8000';
+// prod:
+export const API_BASE_URL = 'https://api-backend-rho-vert.vercel.app';
+
 export const API_URL = `${API_BASE_URL}/api/v1`;
